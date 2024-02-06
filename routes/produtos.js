729cab04d312dd12170d53b2dd42b6d196c50f2d
Lucas <lucas.ferreira@blueshift.com.br>
2024-02-06 16:12:49 -0300

    upload de fotos

--- routes/produtos.js
+++ routes/produtos.js
@@ -1,6 +1,33 @@
 const express = require("express");
 const router = express.Router();
 const mysql = require('../mysql').pool;
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads');
+    },
+    filename: function( req, file, cb ){
+        let data = new Date().toISOString().replace(/:/g, '-') + '-';
+        cb(null, data + file.originalname ); 
+    }
+})
+
+const imageFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' ) {
+        cb(null, true)
+    } else {
+        cb(null, false)
+    }
+}
+
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 1024 * 1024 * 5 // 5mb
+    },
+    fileFilter: imageFilter
+});
 
 router.get("/", (req, res, next) => {
     mysql.getConnection((error, conn) => {
@@ -17,6 +44,7 @@ router.get("/", (req, res, next) => {
                             id_produto: prod.id_produto,
                             nome: prod.nome,
                             preco: prod.preco,
+                            url: prod.imagem_produto,
                             request: {
                                 tipo: 'GET',
                                 descricao: 'Lista um produto especifico',
@@ -49,6 +77,7 @@ router.get("/:id_produto", (req, res, next) => {
                             id_produto: result[0].id_produto,
                             nome: result[0].nome,
                             preco: result[0].preco,
+                            url: result[0].imagem_produto,
                             request: {
                                 tipo: 'GET',
                                 descricao: 'Lista um produto especifico',
@@ -63,12 +92,12 @@ router.get("/:id_produto", (req, res, next) => {
 });
 
 
-router.post("/", (req, res, next) => {
+router.post("/", upload.single('produto_imagem'), (req, res, next) => {
     mysql.getConnection((error, conn) => {
         if (error) { return res.status(500).send({error: error})}
         conn.query(
-            'INSERT INTO produtos (nome, preco) VALUES (?,?)',
-            [req.body.nome, req.body.preco],
+            'INSERT INTO produtos (nome, preco, imagem_produto) VALUES (?,?, ?)',
+            [req.body.nome, req.body.preco, req.file.path],
             (error, result, fields) => {
                 conn.release();
                 if (error) { return res.status(500).send({ error: error }) }
@@ -78,6 +107,7 @@ router.post("/", (req, res, next) => {
                         id_produto: result.id_produto,
                         nome: req.body.nome,
                         preco: req.body.preco,
+                        url: req.body.path,
                         request: {
                             tipo: 'GET',
                             descricao: 'Lista um produto especifico',
